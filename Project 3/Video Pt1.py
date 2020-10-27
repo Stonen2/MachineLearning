f6a7129f2a8a17c57b94a63ce8abfa2b1c896f85
Nicholas Stone <stonen2@xavier.edu>
2020-10-27 16:52:26 -0600

    Pushing up shell of program

---
+++ Project 3/Video Pt1.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import math, random, copy
+import TestData
+import DataUtility
+import NeuralNetwork
+import matplotlib.pyplot as plt
+import time 
+import Performance
+
+
+
+def main(): 
+    print("=================Video Part 1 STARTING=================")
+    #Show the sample model for smalled of NN types
+    #Demonstrate the Gradient Calculation at the output for a Neural Network     
+    #Demonstrate Weight Updates occurring on a two-layer netwrok for each layer 
+
+
+    #Show how an example is propagates through a 2 hiddent layer network 
+
+
+    print("==================Video Part 1 ENDING==================")
+
+
+
+main() 
---
+++ Project 3/Video Pt2.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import math, random, copy
+import TestData
+import DataUtility
+import NeuralNetwork
+import matplotlib.pyplot as plt
+import time 
+import Performance
+
+
+
+def main(): 
+    print("=================Video Part 2 STARTING=================")
+    #Show the outputs for one test set showing performance show results for each layer 
+    #SHow the performance over the ten folds for one set for each type of network 
+
+   
+
+
+
+    print("==================Video Part 2 ENDING==================")
+
+
+
+main() 
